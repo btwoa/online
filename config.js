6560197b8bbb15040c9f0b949b3cbec04988097d
btwoa <asciiurl@gmail.com>
2022-10-11 22:05:50 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'ONLINE',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1852899-2d88461a36f3a2ecd6538258',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'LINK',
+      url: 'https://blog.btwoa.com/link'
+    },
+    {
+      text: 'BTWOA',
+      url: 'https://btwoa.com/'
+    },
+  ],
+};
